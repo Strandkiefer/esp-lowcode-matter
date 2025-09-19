582fe8c8b47259a70060ffe2e959a3238847bd57
Strandkiefer <131675660+Strandkiefer@users.noreply.github.com>
2025-09-19 11:10:44 +0200

    Fix WindowsCovering build for updated ESP-AMP and LEDC APIs

--- components/light/led/led_driver.c
+++ components/light/led/led_driver.c
@@ -60,7 +60,7 @@ int led_driver_set_channel(uint8_t channel, uint8_t val)
     ledc_ll_set_hpoint(&LEDC, speed_mode, channel, hpoint);
     /* LEDC_CHn_DUTY: config duty */
     ledc_ll_set_duty_int_part(&LEDC, speed_mode, channel, (LEDC_MAX_DUTY * val / 100));
-    ledc_ll_set_duty_start(&LEDC, speed_mode, channel, true);
+    ledc_ll_set_duty_start(&LEDC, speed_mode, channel);
     /* LEDC_PARA_UP_CHn: enable the configuration above */
     if (speed_mode == LEDC_LOW_SPEED_MODE) {
         ledc_ll_ls_channel_update(&LEDC, speed_mode, channel);
@@ -114,7 +114,6 @@ void led_driver_deinit(void)
     for (int ch=LED_CHANNEL_NC+1; ch<LED_CHANNEL_MAX; ch++) {
         if (channel_mask | BIT(ch)) {
             ledc_ll_set_sig_out_en(&LEDC, speed_mode, ch, false);
-            ledc_ll_set_duty_start(&LEDC, speed_mode, ch, false);
             if (speed_mode == LEDC_LOW_SPEED_MODE) {
                 ledc_ll_ls_channel_update(&LEDC, speed_mode, ch);
             }
