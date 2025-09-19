0a57d32a8543203e425b73fd2c2c88a9894cf506
Strandkiefer <131675660+Strandkiefer@users.noreply.github.com>
2025-09-19 14:27:27 +0200

    Invert relay output logic for WindowsCovering

--- products/WindowsCovering/main/app_driver.cpp
+++ products/WindowsCovering/main/app_driver.cpp
@@ -17,111 +17,23 @@
 #include <system.h>
 #include <low_code.h>
 
-#include <button_driver.h>
 #include <relay_driver.h>
-#include <light_driver.h>
+#include <hal/gpio_types.h>
 
 #include "app_priv.h"
 
-#define BUTTON1_GPIO_NUM ((gpio_num_t)9)
-#define BUTTON2_GPIO_NUM ((gpio_num_t)10)
 #define RELAY1_GPIO_NUM ((gpio_num_t)2)
 #define RELAY2_GPIO_NUM ((gpio_num_t)3)
-#define INDICATOR_GPIO_NUM ((gpio_num_t)8)
 
 static const char *TAG = "app_driver";
 
 static bool socket_states[2] = {false, false};
-
-static void app_driver_toggle_socket_state_button_callback(void *arg, void *data)
-{
-    uint8_t endpoint_id = (uint8_t)(uintptr_t)data;
-    uint8_t socket_index = endpoint_id - 1;
-
-    socket_states[socket_index] = !socket_states[socket_index];
-    printf("%s: Set socket %d state to %d\n", TAG, endpoint_id, socket_states[socket_index]);
-    app_driver_set_socket_state(endpoint_id, socket_states[socket_index]);
-
-    low_code_feature_data_t update_data = {
-        .details = {
-            .endpoint_id = endpoint_id,
-            .feature_id = LOW_CODE_FEATURE_ID_POWER
-        },
-        .value = {
-            .type = LOW_CODE_VALUE_TYPE_BOOLEAN,
-            .value_len = sizeof(bool),
-            .value = (uint8_t*)&socket_states[socket_index],
-        },
-    };
-
-    low_code_feature_update_to_system(&update_data);
-}
-
-static void app_driver_trigger_factory_reset_button_callback(void *arg, void *data)
-{
-    /* Update by sending event */
-    low_code_event_t event = {
-        .event_type = LOW_CODE_EVENT_FACTORY_RESET
-    };
-
-    low_code_event_to_system(&event);
-    printf("%s: Factory reset triggered\n", TAG);
-}
 
 int app_driver_init()
 {
     /* Initialize relays */
     relay_driver_init(RELAY1_GPIO_NUM);
     relay_driver_init(RELAY2_GPIO_NUM);
-
-    /* Initialize button 1 */
-    button_config_t btn1_cfg = {
-        .gpio_num = BUTTON1_GPIO_NUM,
-        .pullup_en = 1,
-        .active_level = 0,
-    };
-    button_handle_t btn1_handle = button_driver_create(&btn1_cfg);
-    if (!btn1_handle) {
-        printf("Failed to create button 1\n");
-        return -1;
-    }
-
-    /* Register callbacks for button 1 */
-    button_driver_register_cb(btn1_handle, BUTTON_SINGLE_CLICK, app_driver_toggle_socket_state_button_callback, (void*)1);
-    button_driver_register_cb(btn1_handle, BUTTON_LONG_PRESS_UP, app_driver_trigger_factory_reset_button_callback, NULL);
-
-    /* Initialize button 2 */
-    button_config_t btn2_cfg = {
-        .gpio_num = BUTTON2_GPIO_NUM,
-        .pullup_en = 1,
-        .active_level = 0,
-    };
-    button_handle_t btn2_handle = button_driver_create(&btn2_cfg);
-    if (!btn2_handle) {
-        printf("Failed to create button 2\n");
-        return -1;
-    }
-
-    /* Register callbacks for button 2 */
-    button_driver_register_cb(btn2_handle, BUTTON_SINGLE_CLICK, app_driver_toggle_socket_state_button_callback, (void*)2);
-    button_driver_register_cb(btn2_handle, BUTTON_LONG_PRESS_UP, app_driver_trigger_factory_reset_button_callback, NULL);
-
-    /* Initialize common indicator */
-    light_driver_config_t cfg = {
-        .device_type = LIGHT_DEVICE_TYPE_WS2812,
-        .channel_comb = LIGHT_CHANNEL_COMB_3CH_RGB,
-        .io_conf = {
-            .ws2812_io = {
-                .ctrl_io = INDICATOR_GPIO_NUM,
-            },
-        },
-        .min_brightness = 0,
-        .max_brightness = 100,
-    };
-    light_driver_init(&cfg);
-
-    /* Set initial LED states */
-    light_driver_set_power(socket_states[0] || socket_states[1]);
 
     printf("%s: App driver initialized\n", TAG);
     return 0;
@@ -135,10 +47,7 @@ int app_driver_set_socket_state(uint16_t endpoint_id, bool state)
 
     /* Set appropriate relay */
     gpio_num_t relay_gpio = (endpoint_id == 1) ? RELAY1_GPIO_NUM : RELAY2_GPIO_NUM;
-    relay_driver_set_power(relay_gpio, state);
-
-    bool any_socket_on = socket_states[0] || socket_states[1];
-    light_driver_set_power(any_socket_on);
+    relay_driver_set_power(relay_gpio, !state);
 
     return 0;
 }
@@ -147,25 +56,13 @@ int app_driver_event_handler(low_code_event_t *event)
 {
     /* Get the events. Approriate indicators should be shown to the user based on the event. */
     printf("%s: Received event: %d\n", TAG, event->event_type);
-    light_effect_config_t effect_config = {
-        .type = LIGHT_EFFECT_INVALID,
-        .mode = LIGHT_WORK_MODE_COLOR, /* Since it is a single channel LED */
-        .max_brightness = 100,
-        .min_brightness = 10
-    };
-
     /* Handle the events from low_code_event_type_t */
     switch (event->event_type) {
         case LOW_CODE_EVENT_SETUP_MODE_START:
             printf("%s: Setup mode started\n", TAG);
-            /* Start Indication */
-            effect_config.type = LIGHT_EFFECT_BLINK;
-            light_driver_effect_start(&effect_config, 2000, 120000);
             break;
         case LOW_CODE_EVENT_SETUP_MODE_END:
             printf("%s: Setup mode ended\n", TAG);
-            /* Stop Indication */
-            light_driver_effect_stop();
             break;
         case LOW_CODE_EVENT_SETUP_DEVICE_CONNECTED:
             printf("%s: Device connected during setup\n", TAG);
